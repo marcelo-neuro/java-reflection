824e77a1778b890ca04f91169dbb6bec5dceb8ed
marcelo-neuro <marcelofurla0813@gmail.com>
2025-10-13 16:42:14 -0300

    method iteration created

--- src/main/java/br/com/marcelo_neuro/refl/Transformator.java
+++ src/main/java/br/com/marcelo_neuro/refl/Transformator.java
@@ -1,5 +1,6 @@
 package br.com.marcelo_neuro.refl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Transformator {
@@ -10,6 +11,21 @@ public class Transformator {
 
         U instanceOfTarget = (U) targetClass.getDeclaredConstructor().newInstance();
 
-        return null;
+        Field[] sourceFields = sourceClass.getDeclaredFields();
+        Field[] targetFields = targetClass.getDeclaredFields();
+
+        int sourceFieldI = 0;
+        int targetFieldI = 0;
+        while (targetFieldI < targetFields.length && sourceFieldI < sourceFields.length) {
+            if(targetFields[targetFieldI].getName().equals(sourceFields[sourceFieldI].getName())) {
+                targetFields[targetFieldI].setAccessible(true);
+
+                targetFieldI++;
+            } else {
+                sourceFieldI++;
+            }
+        }
+
+        return instanceOfTarget;
     }
 }
